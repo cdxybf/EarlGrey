ca3cddaf07cb06ac02546b318f4b73b75160bc78
Rosman Cheng <1064265984@qq.com>
2022-05-19 20:17:48 +1200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "EarlGrey",
+    products: [
+        .library(
+            name: "EarlGrey",
+            targets: ["EarlGrey"]),
+    ],
+    targets: [
+        .target(
+            name: "EarlGrey",
+            dependencies: [],
+            path: "EarlGrey"),
+        .testTarget(
+            name: "Tests",
+            dependencies: ["EarlGrey"],
+            path: "Tests"),
+    ]
+)
